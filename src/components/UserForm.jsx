89089fff9d6b0f92e1aa0ef16cb6d81bf3cd697a
belyseing <162991256+belyseing@users.noreply.github.com>
2025-05-01 15:54:43 +0200

    updated React form

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -18,8 +18,8 @@ function UserForm() {
     alert("Form submiited");
   }
   return (
-    <div className="h-screen w-full bg-gradient-to-r from-slate-300  flex items-center justify-center">
-      <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-md">
+    <div className="h-screen w-full bg-gradient-to-r from-slate-300  flex items-center justify-center ">
+      <div className="bg-white p-8 sm:p-6 md:p-8 rounded-xl shadow-lg w-[90%] max-w-sm sm:max-w-md">
         <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
           Fill this Form
         </h1>
